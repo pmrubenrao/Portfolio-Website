8f4da36b0cf18696aecb05892fd664af929a4100
ruben rao <pmrubenrao@gmail.com>
2021-11-20 23:06:18 +0530

    Re uploaded the screenshot of View port page

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router.js';
-import LoadScript from 'vue-plugin-load-script';
 
 const app = createApp(App);
 app.use(router);
 router.isReady().then(function() {
   app.mount('#app');
 });
-app.use(LoadScript);
